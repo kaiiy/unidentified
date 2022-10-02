fc4d14f02dd5de979e24fe3aa36b22a4535b7c45
kaiiy <kaiiy@users.noreply.github.com>
2022-10-02 13:35:05 +0000

    deploy: 3ac158afd5d3310c1ebf5e316d0c454ab7fd01d1

---
+++ _nuxt/bed74d6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{615:function(e,n,t){"use strict";t.r(n);var l=t(76),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",{staticStyle:{"{ pointer-events":"none"}},[e._v("〝")])}),[],!1,null,null,null);n.default=component.exports}}]);
